b1c70bd5536d07fdaaafe6406fca13fd70847a8e
Rafal Wadolowski <rafal@rafal.net.pl>
2015-01-13 02:28:00 +0100

    morse init

---
+++ morse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -A mors
+mors[a]=.-
+mors[b]=-...
+mors[c]=-.-.
+
+wejscie=$1
+
+inputlen=${#wejscie}
+gora=""
+dol=""
+for(( i=0;i<$inputlen;i++));
+do
+wielkosc=${#mors[${wejscie:$i:1}]}
+gora=$gora${mors[${wejscie:$i:1}]}" "
+dol=$dol${wejscie:$i:1}
+for(( ii=0;ii<$wielkosc;ii++));
+do
+echo $ii
+dol=$dol" "
+done
+done
+echo $gora
+echo "${dol}"
